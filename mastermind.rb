53268493071bd01ffba1e19c7b3f2d736dd062b1
Everesh <everesh@proton.me>
2024-05-08 02:30:57 +0200

    Implement computer_mastermind

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,8 @@
 class Mastermind
   @code = Array.new(4)
   @history = [] # Im thinking 3d array of [x][2][4], where x is the number of guesses
+  COLORS = %w[white black red green blue yellow orange purple].freeze # 8 colors total
+  ATTEMPTS = 8
 
   def initialize
     puts ' ███▄ ▄███▓ ▄▄▄        ██████ ▄▄▄█████▓▓█████  ██▀███   ███▄ ▄███▓ ██▓ ███▄    █ ▓█████▄ '
@@ -9,23 +11,69 @@ class Mastermind
     puts '▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ▒██    ▒██ ░██░▓██▒  ▐▌██▒░▓█▄   ▌'
     puts '▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒▒██▒   ░██▒░██░▒██░   ▓██░░▒████▓ '
     puts '░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░   ░  ░░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒ '
-    computer_mastermind? ? init_computer_mastermind : init_player_mastermind
+    new_game
   end
 
   private
 
-  def computer_mastermind?
-    puts 'Computer mastermind? [Y/n]'
-    return true unless gets.chr.downcase == 'n'
+  attr_accessor :code, :history
 
+  def new_game
+    puts 'Computer mastermind? [Y/n]'
+    if gets.chr.downcase == 'n'
+      computer_mastermind
+    else
+      player_mastermind
+    end
+  end
+
+  def new_game?
+    puts 'Care for a rematch? [Y/n]'
+    if gets.chr.downcase == 'n'
+      false
+    else
+      true
+    end
+  end
+
+  def computer_mastermind
+    code.map! { COLORS[rand(COLORS.lenght)] }
+    ATTEMPTS.times { break if guessed? }
+    print_state
+    if matching?
+      puts 'You did it, you won! The computer got mastered or something'
+    else
+      puts 'Shucks, better luck next time'
+    end
+    new_game if new_game?
+  end
+
+  def guessed?
+    print_state
+    take_guess
+    feedback
+    true if matching?
     false
   end
 
-  def init_computer_mastermind
+  def take_guess
     # TO DO
   end
 
-  def init_player_mastermind
+  def feedback
+    # TO DO
+  end
+
+  def matching?
+    # TO DO
+  end
+
+  def player_mastermind
+    # TO DO
+    new_game if new_game?
+  end
+
+  def print_state
     # TO DO
   end
 end
